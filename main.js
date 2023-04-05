b25c6e0b19fb9bcaba9ae482772f1196a5d59dbc
EDuardoMErlinDUSK <129889923+EDuardoMErlinDUSK@users.noreply.github.com>
2023-04-05 12:06:37 -0300

    a

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function exiberesultado()
     let calculo1 = document.getElementById( "varialvel1").value;
     let calculo2 = document.getElementById( "varialvel2").value;
     document.getElementById("resultado").innerHTML = value + value;
-    document.getElementById( "calculo1").value = "";
-    document.getElementById( "calculo2").value = "";
+    document.getElementById( "varialvel1").value = "";
+    document.getElementById( "varialvel2").value = "";
 }
 
