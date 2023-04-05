d4f46b08eb780195580c1dd4784cdcd9cc2b7884
EDuardoMErlinDUSK <129889923+EDuardoMErlinDUSK@users.noreply.github.com>
2023-04-05 12:35:41 -0300

    dividir

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener( "click" , showcolor );
 
 document.getElementById( "botao"). addEventListener( "click" , exibenome );
 document.getElementById( "botao2"). addEventListener( "click" , exiberesultado);
+document.getElementById( "botao3"). addEventListener( "click" , exiberesultado);
 
 function showcolor()
 {
@@ -17,6 +18,41 @@ function exibenome()
 
 
 function exiberesultado()
+{
+    let calculo1 = document.getElementById( "variavel1").value;
+    let calculo2 = document.getElementById( "variavel2").value;
+    document.getElementById("resultado").innerHTML =  parseFloat(calculo1) + parseFloat(calculo2);
+    document.getElementById( "variavel1").value = "";
+    document.getElementById( "variavel2").value = "";
+}
+
+
+
+
+function exiberesultado2()
+{
+    let divirlegal1 = document.getElementById( "dividir1").value;
+    let divirlegal2 = document.getElementById( "dividir2").value;
+    document.getElementById("resultado").innerHTML =  parseFloat(divirlegal1) / parseFloat(divirlegal2);
+    document.getElementById( "dividir1").value = "";
+    document.getElementById( "dividir2").value = "";
+}
+
+
+
+function exiberesultado3()
+{
+    let calculo1 = document.getElementById( "variavel1").value;
+    let calculo2 = document.getElementById( "variavel2").value;
+    document.getElementById("resultado").innerHTML =  parseFloat(calculo1) + parseFloat(calculo2);
+    document.getElementById( "variavel1").value = "";
+    document.getElementById( "variavel2").value = "";
+}
+
+
+
+
+function exiberesultado4()
 {
     let calculo1 = document.getElementById( "variavel1").value;
     let calculo2 = document.getElementById( "variavel2").value;
